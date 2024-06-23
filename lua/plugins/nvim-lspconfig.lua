cad2f43ae4efd99512e8f32491514f311509d9df
Brady Goenner <bradygoenner@gmail.com>
2024-06-22 19:22:03 -0600

    lspconfig fix

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,7 @@
 local config = function()
   local lspconfig = require("lspconfig")
 
-  local signs = { Error = "(x)", Warn = "(!)", Hint = "+ ", Info = "(i)" }
+  local signs = { Error = "x", Warn = "!", Hint = "+ ", Info = "i" }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text=icon, texthl = hl, numhl = "" })
